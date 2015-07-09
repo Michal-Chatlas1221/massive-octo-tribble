8f4ee3b49fd7b3aaeb5472d733f237b8cfd8017a
Zuza Sawala <vekka15@gmail.com>
2015-07-09 22:46:35 +0200

    triangle

--- czołgi/main_file.cpp
+++ czołgi/main_file.cpp
@@ -6,9 +6,55 @@
 #include "glm/gtc/type_ptr.hpp"
 #include <stdio.h>
 
+glm::mat4 V;
+glm::mat4 P;
+glm::mat4 M;
+
+//Trójk¹t 3 wierzcho³ki
+static const GLfloat g_vertex_buffer_data[] = {
+	-1.0f, -1.0f, 0.0f,
+	1.0f, -1.0f, 0.0f,
+	0.0f, 1.0f, 0.0f,
+};
+
+void drawObject(){
+	GLuint vertexbuffer;
+	glGenBuffers(1, &vertexbuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+	glVertexAttribPointer(
+		0,                  // attribute 0. No particular reason for 0, but must match the layout in the shader.
+		3,                  // size
+		GL_FLOAT,           // type
+		GL_FALSE,           // normalized?
+		0,                  // stride
+		(void*)0            // array buffer offset
+		);
+
+	// Draw the triangle !
+	glDrawArrays(GL_TRIANGLES, 0, 3); // Starting from vertex 0; 3 vertices total -> 1 triangle
+
+	glDisableVertexAttribArray(0);
+}
+
+
 void displayFrame() {
-	glClearColor(0, 0, 0, 1);
+	glClearColor(1, 0, 0, 1); //Zmienia kolor t³a
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Widok
+	V = glm::lookAt(glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+
+	//Rzutowanie
+	P = glm::perspective(45.0f, 100.0f, 1.0f, 100.0f);
+
+	//Model
+	M = glm::rotate(glm::mat4(1.0f), 0.0f, glm::vec3(1, 0, 0));
+
+	drawObject();
+
 	glutSwapBuffers();
 }
 
@@ -16,7 +62,7 @@ void initializeGLUT(int* pargc, char** argv) {
 	glutInit(pargc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
 	glutInitWindowPosition(0, 0);
-	glutInitWindowSize(800, 800);
+	glutInitWindowSize(400, 400);
 	glutCreateWindow("Program OpenGL");
 	glutDisplayFunc(displayFrame);
 }
@@ -33,8 +79,8 @@ void initializeGLEW() {
 
 
 int main(int argc, char** argv) {
-	initializeGLEW();
 	initializeGLUT(&argc, argv);
+	initializeGLEW();
 	//Kod inicjuj¹cy tutaj
 	glutMainLoop();
 	//Kod zwalniaj¹cy zasoby tutaj
